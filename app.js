c8c077be4c4fa870c89ad612b6a1b5880bb0e449
Sam Goldman <samwgoldman@gmail.com>
2011-11-10 23:06:25 -0500

    Add stanford parser gui screenshot

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 $(function() {
-  var edit_url = "https://github.com/samwgoldman/samwgoldman.github.com/edit/master" + location.pathname;
-  var link = $("<a href='" + edit_url + "'>Edit this page</a>");
-
   $("article > h2").each(function(index, elem) {
-    link.insertAfter(elem);
+    var edit_url = "https://github.com/samwgoldman/samwgoldman.github.com/edit/master" + location.pathname;
+    $("<a href='" + edit_url + "'>Edit this page</a>").insertAfter(elem);
+  });
+
+  $("article .screenshot").each(function(index, elem) {
+    var image = $("img", elem)
+      .load(function() { image.width(this.width); image.height(this.height); })
+      .hide()
+      .addClass('modal fade')
+      .modal({ "backdrop": true, "keyboard": true });
+    $("<a class='btn small'>View screenshot</a>")
+      .prependTo(elem)
+      .click(function() { image.modal('toggle'); });
   });
 });
